e4122ebdcf552ec393d610a496466835eb287f97
Andrés <carlosraxon019@gmail.com>
2025-08-24 12:44:27 -0600

    remove unused files

--- lib/types/projects.ts
+++
@@ -1,3 +0,0 @@
-import type { PROJECTS } from '@/lib/constants/projects'
-
-export type Proyect = (typeof PROJECTS)[keyof typeof PROJECTS]
--- lib/types/theme.ts
+++
@@ -1,7 +0,0 @@
-import type { VariantProps } from 'class-variance-authority'
-
-type ExcludeNull<T> = { [P in keyof T]: Exclude<T[P], null> }
-
-export type NonNullableVariantProps<Comp extends (...args: never) => unknown> = ExcludeNull<
-  VariantProps<Comp>
->

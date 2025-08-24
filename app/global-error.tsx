764d654ff84bd81c44da1f6df231e3b42c3f1112
Andrés <carlosraxon019@gmail.com>
2025-08-23 21:17:42 -0600

    fix heading size on side pages

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -22,8 +22,12 @@ export default function ErrorPage({
       <body className={`${fontHeading.variable} ${fontBody.variable} antialiased`}>
         <main className='container mx-auto w-11/12'>
           <div className='isolate flex h-dvh flex-col items-center justify-center gap-3'>
-            <span className='text-muted-foreground text-lg leading-none'>500</span>
-            <h1 className='font-heading text-center text-6xl'>Error Crítico</h1>
+            <span className='text-muted-foreground tablet:text-xl desktop:text-2xl text-lg leading-none'>
+              500
+            </span>
+            <h1 className='font-heading tablet:text-7xl desktop:text-8xl text-center text-6xl'>
+              Error Crítico
+            </h1>
             <Button className='mt-2' onClick={reset}>
               Volver a intentar <IconRefresh className='size-4' />
             </Button>

dc814208dd1183b87ba40164b7e7811c4e9afa79
Andrés <carlosraxon019@gmail.com>
2025-08-25 18:24:48 -0600

    update favicons url to cloudinary

--- lib/constants/site.ts
+++ lib/constants/site.ts
@@ -11,14 +11,18 @@ export const SITE = {
       SITEMAP: '/sitemap.xml',
     },
     IMAGES: {
-      BANNER_SOCIAL: '/banner-og.png',
-      SYMBOL: '/brand/symbol.svg',
+      BANNER_SOCIAL:
+        'https://res.cloudinary.com/dcopojqng/image/upload/v1756165838/banner-og_x5ds5e.png',
+      SYMBOL: 'https://res.cloudinary.com/dcopojqng/image/upload/v1755992561/symbol_noz7xs.svg',
       ICON: {
-        ICO_SVG: '/favicon.svg',
-        APPLE: '/apple-touch-icon.png',
+        ICO_SVG: 'https://res.cloudinary.com/dcopojqng/image/upload/v1755992561/symbol_noz7xs.svg',
+        APPLE:
+          'https://res.cloudinary.com/dcopojqng/image/upload/v1756165838/apple-touch-icon_fvport.png',
         MANIFEST: {
-          ICO_192: '/web-app-manifest-192x192.png',
-          ICO_512: '/web-app-manifest-512x512.png',
+          ICO_192:
+            'https://res.cloudinary.com/dcopojqng/image/upload/v1756165838/web-app-manifest-192x192_igbr0z.png',
+          ICO_512:
+            'https://res.cloudinary.com/dcopojqng/image/upload/v1756165838/web-app-manifest-512x512_gg8no7.png',
         },
       },
     },

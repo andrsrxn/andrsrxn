3b8747a039687bd1e1dd4aa61bf9d30df045ab95
Andrés <carlosraxon019@gmail.com>
2025-12-02 15:08:03 -0600

    feat: add project descriptions

--- lib/constants/projects.ts
+++ lib/constants/projects.ts
@@ -7,6 +7,10 @@ export const PROJECTS = {
     CLIENT: 'Grupo ATProy',
     INDUSTRY: 'Ingeniería eléctrica y telecomunicación',
     SUMMARY: 'Actualización de Marca y Desarrollo Web',
+    DESCRIPTION: [
+      'Con un trayectoria de más de dos décadas en el sector de Ingeniería eléctrica y Telecomunicaciones, Grupo ATProy realiza proyectos de gran magnitud a lo largo de Centroamérica.',
+      'Ante la necesidad de que su Identidad Visual reflejara la calidad y estándares de excelencia, refinamos y establecimos consistencia en su marca acompañado de la creación del sitio web, con el objetivo de facilitar la información de sus servicios y proyectos realizados a lo largo de su historia y un medio de contacto para crecer su experiencia con más clientes.',
+    ],
     SERVICES: [
       SERVICES.BRAND.SUBSERVICES.BRAND_REFRESH.TITLE,
       SERVICES.BRAND.SUBSERVICES.VISUAL_IDENTITY.TITLE,
@@ -20,13 +24,9 @@ export const PROJECTS = {
     CLIENT: 'La Presencia del Señor Jesús',
     INDUSTRY: 'Iglesia evangélica',
     SUMMARY: 'Branding, Desarrollo Web y Redes Sociales',
-    SERVICES: [
-      SERVICES.BRAND.SUBSERVICES.BRANDING.TITLE,
-      SERVICES.MARKETING.SUBSERVICES.SOCIAL_MEDIA_POSTS.TITLE,
-      SERVICES.MARKETING.SUBSERVICES.COPYWRITING.TITLE,
-      SERVICES.MARKETING.SUBSERVICES.MOTION_GRAPHICS.TITLE,
-      SERVICES.WEB_DEV.SUBSERVICES.SEO.TITLE,
-      SERVICES.WEB_DEV.SUBSERVICES.FULL_PAGE_DEV.TITLE,
+    DESCRIPTION: [
+      'Para el establecimiento de su iglesia y comunidad, desarrollamos desde cero la marca creando un sistema visual coherente que reflejara su esencia en sus principales canales de comunicación.',
+      'Desarrollamos un Sitio web (Blog) en inglés y español, diseñado para centralizar la información clave de la iglesia, ser una herramienta de calendarización, oferecer detalles sobre los servicios y acceso a estudios bíblicos relevantes.',
     ],
     IMAGES: IMAGES.PROJECTS.ICPSJ,
   },
@@ -36,6 +36,10 @@ export const PROJECTS = {
     CLIENT: 'Bonzer',
     INDUSTRY: 'Estética y cuidado personal',
     SUMMARY: 'Branding y Posts para Redes Sociales',
+    DESCRIPTION: [
+      'Bonzer es un concepto de marca enfocada en productos naturales para el cuidado y bienestar personal en jóvenes y adultos, generando cambios visibles en poco tiempo.',
+      'El Branding realizado permite enfatizar la personalidad vibrante y cercana de Bonzer, aumentando la fidelidad del mercado y facilidad de reconocimiento con respecto a la competencia, principalmente en los elementos gráficos que acompañan los distintos medios de publicidad.',
+    ],
     SERVICES: [
       SERVICES.BRAND.SUBSERVICES.BRANDING.TITLE,
       SERVICES.MARKETING.SUBSERVICES.SOCIAL_MEDIA_POSTS.TITLE,
@@ -47,6 +51,10 @@ export const PROJECTS = {
     CLIENT: 'ICEMENE',
     INDUSTRY: 'Joyería y accesorios',
     SUMMARY: 'Propuesta de Rebranding',
+    DESCRIPTION: [
+      'Mejoramos la calidad gráfica de ICEMENE, una tienda de joyería y accesorios masculinos, alineando su personalidad con la marca para reflejar la elegancia y calidad que lo caracteriza.',
+      'Por medio del Rebranding actualizamos la estretegia de marca que impulsará sus decisiones a futuro más allá del diseño, tanto digital y en medios físicos.',
+    ],
     SERVICES: [
       SERVICES.BRAND.SUBSERVICES.REBRANDING.TITLE,
       SERVICES.BRAND.SUBSERVICES.BRAND_STRATEGY.TITLE,
@@ -59,6 +67,11 @@ export const PROJECTS = {
     CLIENT: 'ThePrincipal',
     INDUSTRY: 'Blog online',
     SUMMARY: 'Branding y Diseño UI/UX',
+    DESCRIPTION: [
+      'ThePrincipal es una plataforma en línea de auto aprendizaje inspirada en la lectura de los periódicos combinada con la interación dinámico de los usuarios en la web.',
+      'El valor del conocimiento online debe estar acompañado de una marca que respalde la experiencia de aprender y descubrir.',
+      'La plataforma inlcuye cursos, artículos, resúmenes de temas relevantes e históricos y herramientas en línea; facilita el seguimiento de los cursos, la publicación automatizada de los temas, búsqueda avanzada por categorías, entre otros, totalmente gratis.',
+    ],
     SERVICES: [
       SERVICES.BRAND.SUBSERVICES.BRANDING.TITLE,
       SERVICES.BRAND.SUBSERVICES.VISUAL_IDENTITY.TITLE,
@@ -73,6 +86,11 @@ export const PROJECTS = {
     CLIENT: 'Solar Group Guatemala',
     INDUSTRY: 'Ingeniería eléctrica',
     SUMMARY: 'Propuesta de Rebranding',
+    DESCRIPTION: [
+      'Creamos una nueva oportunidad de crecimiento en el mercado para Solar Group Guatemala con una marca consistente y fácilmente reconocible en todas sus aplicaciones.',
+      'La marca se distingue por medio de la innovación, claridad del mensaje y reconocimiento constante, proporcionandole un mejor posicionamiento en el mercado.',
+      'Nos concentramos en revitalizar su Identidad Visual creando un universo gráfico sólido y coherente que destaque sus servicios junto con su amplia trayectoria, adaptándola a las necesidades modernas de su industria.',
+    ],
     SERVICES: [
       SERVICES.BRAND.SUBSERVICES.REBRANDING.TITLE,
       SERVICES.BRAND.SUBSERVICES.VISUAL_IDENTITY.TITLE,
@@ -85,6 +103,10 @@ export const PROJECTS = {
     CLIENT: 'Transportii S.A.',
     INDUSTRY: 'Ingeniería eléctrica y telecomunicaciones',
     SUMMARY: 'Nueva Identidad Visual',
+    DESCRIPTION: [
+      'Colaboramos con Transportii S.A., una de las empresas de ingeniería eléctrica más importantes de Guatemala, quienes buscan refinar y modernizar su marca sin perder reconocimiento.',
+      'El avance tecnológico de su industria demanda mantenerse en constante actualización, por lo que garantizamos que su marca se mantenga al margen de la innovación y crezca integralmente en el futuro.',
+    ],
     SERVICES: [
       SERVICES.BRAND.SUBSERVICES.BRAND_REFRESH.TITLE,
       SERVICES.BRAND.SUBSERVICES.VISUAL_IDENTITY.TITLE,
@@ -97,6 +119,10 @@ export const PROJECTS = {
     CLIENT: 'Pinalza',
     INDUSTRY: 'Arquitectura y construcción',
     SUMMARY: 'Propuesta de Identidad Visual',
+    DESCRIPTION: [
+      'Desarrollamos dos propuestas de Identidad Visual para comunicar las dimensiones profesionales de sus proyectos para generar confianza en clientes potenciales.',
+      'Este es el paso esencial para el desarrollo completo del Rebranding en el futuro.',
+    ],
     SERVICES: [SERVICES.BRAND.SUBSERVICES.VISUAL_IDENTITY.TITLE],
     IMAGES: IMAGES.PROJECTS.PINALZA,
   },

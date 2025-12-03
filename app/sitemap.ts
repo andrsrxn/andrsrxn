d97a71e1e08d3c68686f6f35734a84331ed5fcc4
Andrés <carlosraxon019@gmail.com>
2025-12-02 21:03:22 -0600

    fix: update sitemap date and robots api

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,7 +20,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: SITE.BASE_URL,
-      lastModified: new Date('2025-08-17').toISOString(),
+      lastModified: new Date('2025-12-02').toISOString(),
       changeFrequency: 'monthly',
       priority: 1,
     },

854b6a9354ab7966e6d7b534dac803386fd5bdd4
Andrés <carlosraxon019@gmail.com>
2026-01-27 08:54:19 -0600

    feat: add cloudflare scripts to csp

--- proxy.ts
+++ proxy.ts
@@ -3,10 +3,12 @@ import { isProductionEnv } from '@/lib/config/env'
 
 export default function proxy(request: NextRequest) {
   const styleSrc = "'self' 'unsafe-inline'"
-  let scriptSrc = "'self' 'unsafe-inline' https://va.vercel-scripts.com"
+  let scriptSrc =
+    "'self' 'unsafe-inline' https://va.vercel-scripts.com https://static.cloudflareinsights.com"
   const imgSrc = "'self' blob: data: https://res.cloudinary.com"
   let workerSrc = "'self'"
-  const connectSrc = "'self' https://va.vercel-scripts.com https://res.cloudinary.com"
+  const connectSrc =
+    "'self' https://va.vercel-scripts.com https://res.cloudinary.com https://static.cloudflareinsights.com"
 
   if (!isProductionEnv()) {
     scriptSrc += " 'unsafe-eval'"

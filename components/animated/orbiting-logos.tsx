3237991baaa0e8cf89344d2501f7c4b241c81625
Andrés <carlosraxon019@gmail.com>
2026-03-16 11:23:03 -0600

    feat: update orbiting logos for new tools

--- components/animated/orbiting-logos.tsx
+++ components/animated/orbiting-logos.tsx
@@ -10,15 +10,15 @@ export const OrbitingLogos = () => {
   const isLaptop = useMediaQuery('(min-width: 64rem)')
   const isDesktop = useMediaQuery('(min-width: 80rem)')
 
-  const secondCircleRadius = isDesktop ? 530 : isLaptop ? 500 : isTablet ? 400 : 320
-  const firstCircleRadius = isDesktop ? 320 : isLaptop ? 300 : isTablet ? 250 : 200
+  const secondCircleRadius = isDesktop ? 550 : isLaptop ? 500 : isTablet ? 400 : 320
+  const firstCircleRadius = isDesktop ? 330 : isLaptop ? 300 : isTablet ? 250 : 200
 
   const designToolsArray = Object.values(DESIGN_TOOLS)
 
   return (
     <div className='desktop:h-[750px] relative z-50 flex h-[700px] w-full flex-col items-center justify-center overflow-hidden'>
       <OrbitingCircles speed={0.2} radius={secondCircleRadius} iconSize={isLaptop ? 75 : 60}>
-        {designToolsArray.slice(9, designToolsArray.length).map(tool => {
+        {designToolsArray.slice(10, designToolsArray.length).map(tool => {
           return (
             <div key={tool.LABEL} title={tool.LABEL}>
               <tool.ICON
@@ -40,7 +40,7 @@ export const OrbitingLogos = () => {
         radius={firstCircleRadius}
         iconSize={isLaptop ? 75 : 60}
         reverse>
-        {designToolsArray.slice(0, 9).map(tool => {
+        {designToolsArray.slice(0, 10).map(tool => {
           return (
             <div key={tool.LABEL} title={tool.LABEL}>
               <tool.ICON

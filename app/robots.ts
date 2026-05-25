ebce2ed8b97c3dab8895e22eb085b2e118f4adf1
Andrés <carlosraxon019@gmail.com>
2026-05-25 13:08:55 -0600

    chore: disallow static files on robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/*.pdf$', '/*.svg$', '/*.png$'],
     },
     sitemap: `${SITE.BASE_URL}/sitemap.xml`,
   }

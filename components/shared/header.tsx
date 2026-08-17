74b3727fa899eb5e9778ae633abaa5ad0e249e0d
Andrés <carlosraxon019@gmail.com>
2026-08-17 10:18:44 -0600

    feat: add resources link

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,11 +1,27 @@
+import { getTranslations } from 'next-intl/server'
 import { LanguageSwitcher } from '@/components/shared/language-switcher'
+import { Button } from '@/components/ui/button'
 import { Image } from '@/components/ui/image'
 import { Link } from '@/i18n/navigation'
 import { IMAGES } from '@/lib/constants/paths'
+import { cn } from '@/lib/utils'
 
-export const Header = () => {
+export const Header = async () => {
+  const tNav = await getTranslations('homePage.navigation')
+
   return (
     <header className='fixed top-0 left-0 z-1000 flex h-12 w-full items-center justify-center mix-blend-difference'>
+      <Button
+        variant='ghost'
+        size='sm'
+        asChild
+        className={cn(
+          'absolute top-4 left-4 z-1000 h-8 rounded-none px-3 text-xs font-semibold tracking-widest'
+        )}>
+        <a href='https://resources.andrsrxn.com' target='_blank' rel='noopener noreferrer'>
+          {tNav('resources')}
+        </a>
+      </Button>
       <Link href='/' aria-label='Ir al inicio'>
         <Image
           includePlaceholder={false}
